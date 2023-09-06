1ee093f758474a9414d5cce47bd5cb8bbd90a2bb
chaehaeun <chae_3210@naver.com>
2023-09-06 13:46:32 +0900

    [Feat] 검색 키워드 캐싱 기능 추가, 내부 작동 함수 프라이빗 필드 설정

--- src/api/httpClient.ts
+++ src/api/httpClient.ts
@@ -1,3 +1,4 @@
+import { CACHE_EXPIRE_TIME, CACHE_KEY } from "@/constants";
 import axios, { AxiosInstance } from "axios";
 
 type SickItem = {
@@ -6,21 +7,46 @@ type SickItem = {
 };
 
 class HttpService {
-  private httpClient: AxiosInstance;
+  #httpClient: AxiosInstance;
 
   constructor() {
-    this.httpClient = axios.create({
+    this.#httpClient = axios.create({
       baseURL: "http://localhost:4000",
     });
   }
 
+  #getCacheData = () => {
+    const cacheData = sessionStorage.getItem(CACHE_KEY);
+    return cacheData ? JSON.parse(cacheData) : {};
+  };
+
+  #setCacheData = (keyword: string, data: string[]) => {
+    const currentCache = this.#getCacheData();
+    currentCache[keyword] = {
+      data,
+      timestamp: Date.now(),
+    };
+    sessionStorage.setItem(CACHE_KEY, JSON.stringify(currentCache));
+  };
+
   search = async (keyword: string): Promise<string[]> => {
     if (keyword.trim().length === 0) return [];
 
+    const cache = this.#getCacheData();
+
+    if (
+      cache[keyword] &&
+      Date.now() - cache[keyword].timestamp < CACHE_EXPIRE_TIME
+    ) {
+      return cache[keyword].data;
+    }
+
     const params = { q: keyword };
     console.info("calling api");
-    const response = await this.httpClient.get("/sick", { params });
+    const response = await this.#httpClient.get("/sick", { params });
     const sickNmArray = response.data.map((item: SickItem) => item.sickNm);
+
+    this.#setCacheData(keyword, sickNmArray);
 
     return sickNmArray;
   };
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,2 +1,4 @@
 export const TITLE = "국내 모든 임상시험 검색하고<br>온라인으로 참여하기";
 export const DEBOUNCE_TIME = 300;
+export const CACHE_EXPIRE_TIME = 10 * 60 * 1000;
+export const CACHE_KEY = "search_cache";
