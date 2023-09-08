6a21ded733d797206394ee2cfb3b056ffc4f47a5
chaehaeun <chae_3210@naver.com>
2023-09-08 15:53:34 +0900

    [Feat] json server 추가

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,5 +2,5 @@ export const TITLE = "국내 모든 임상시험 검색하고<br>온라인으로
 export const DEBOUNCE_TIME = 300;
 export const CACHE_EXPIRE_TIME = 10 * 60 * 1000;
 export const CACHE_KEY = "search_cache";
-export const BASE_URL = "http://localhost:4000";
+export const BASE_URL = import.meta.env.VITE_APP_BASE_URL;
 export const END_POINT = "/sick";
